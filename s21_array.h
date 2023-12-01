56c9e4c9ed42ebb13fbdd7c98933bec4b83d064d
lana-cnmd <133262813+lana-cnmd@users.noreply.github.com>
2023-12-01 23:32:10 +0300

    for lapushka

--- s21_array.h
+++ s21_array.h
@@ -1,4 +1,97 @@
 #ifndef CPP2_S21_CONTEINERS_1_S21_ARRAY_H
 #define CPP2_S21_CONTEINERS_1_S21_ARRAY_H
 
+#include <iostream>
+
+namespace s21
+{
+    template <typename T, size_t N>
+    struct array
+    {
+        using value_type = T;
+        using reference = T &;
+        using const_reference = const T &;
+        using iterator = T *;
+        using const_iterator = const T *;
+        using size_type = size_t;
+
+        array() = default;
+
+        array(std::initializer_list<value_type> const &items)
+        {
+            size_t i = 0;
+            for (auto it = items.begin(); it < items.end(); ++it)
+            {
+                arr_[i++] = *it;
+            }
+        }
+
+        array(const array &a)
+        {
+            for (size_t i = a.begin(); i < a.end(); ++i)
+            {
+                arr_[i] = a.arr_[i];
+            }
+        }
+
+        array(array &&a) : array(a) {}
+
+        array operator=(array &&a)
+        {
+            if (this != &a)
+            {
+                for (size_t i = a.begin(); i < a.end(); ++i)
+                {
+                    arr_[i] = a.arr_[i];
+                }
+            }
+            return *this;
+        }
+
+        ~array() = default;
+
+        reference at(size_type pos)
+        {
+            if (pos > N - 1 || N == 0)
+            {
+                throw std::out_of_range("Out of range");
+            }
+            return arr_[pos];
+        }
+
+        reference operator[](size_type pos) { return arr_[pos]; }
+
+        const_reference front() { return arr_[0]; }
+
+        const_reference back() { return arr_[N - 1]; }
+
+        iterator data() { return arr_; }
+
+        iterator begin() { return arr_; }
+
+        iterator end() { return arr_ + N; }
+
+        bool empty() { return arr_.begin() == arr_.end(); }
+
+        size_type size() noexcept { return N; }
+
+        size_type max_size() noexcept { return N; }
+
+        void swap(array &other) noexcept
+        {
+            std::swap(arr_, other);
+        }
+
+        void fill(const_reference value)
+        {
+            for (size_t i = arr_.begin(); i < arr_.end(); ++i)
+            {
+                arr_[i] = value;
+            }
+        }
+
+    public:
+        T arr_[N];
+    };
+} // namespaсe s21
 #endif // CPP2_S21_CONTEINERS_1_S21_ARRAY_H
--- test.cc
+++ test.cc
@@ -1,4 +1,87 @@
 #include <gtest/gtest.h>
 
-#include "s21_containersplus.h"
 #include "s21_array.h"
+#include <array>
+
+TEST(Array, IL_Constructor)
+{
+    std::array<int, 3> stdArrInt{1, 2, 3};
+    s21::array<int, 3> arrInt{1, 2, 3};
+    ASSERT_EQ(stdArrInt.size(), arrInt.size());
+    ASSERT_EQ(arrInt[0], 1);
+    ASSERT_EQ(arrInt[1], 2);
+    ASSERT_EQ(arrInt[2], 3);
+
+    std::array<double, 3> stdArrDob{1.1, 2.2, 3.3};
+    s21::array<double, 3> arrDob{1.1, 2.2, 3.3};
+    ASSERT_EQ(stdArrDob.size(), arrDob.size());
+    ASSERT_DOUBLE_EQ(arrDob[0], 1.1);
+    ASSERT_DOUBLE_EQ(arrDob[1], 2.2);
+    ASSERT_DOUBLE_EQ(arrDob[2], 3.3);
+
+    std::array<std::string, 3> stdArrStr{"Hey", ",", "you"};
+    s21::array<std::string, 3> arrStr{"Hey", ",", "you"};
+    ASSERT_EQ(stdArrStr.size(), arrStr.size());
+    EXPECT_EQ(arrStr[0], "Hey");
+    EXPECT_EQ(arrStr[1], ",");
+    EXPECT_EQ(arrStr[2], "you");
+}
+//
+TEST(Array, Move_Constructor)
+{
+    s21::array<int, 3> arrInt{1, 2, 3};
+    s21::array<int, 3> arrInt2 = std::move(arrInt);
+    ASSERT_EQ(arrInt2.size(), 3);
+    ASSERT_EQ(arrInt2[0], 1);
+    ASSERT_EQ(arrInt2[1], 2);
+    ASSERT_EQ(arrInt2[2], 3);
+
+    s21::array<double, 3> arrDob{1.1, 2.2, 3.3};
+    s21::array<double, 3> arrDob2 = std::move(arrDob);
+    ASSERT_EQ(arrDob2.size(), 3);
+    ASSERT_DOUBLE_EQ(arrDob2[0], 1.1);
+    ASSERT_DOUBLE_EQ(arrDob2[1], 2.2);
+    ASSERT_DOUBLE_EQ(arrDob2[2], 3.3);
+
+    s21::array<std::string, 3> arrStr{"Hey", ",", "you"};
+    s21::array<std::string, 3> arrStr2 = std::move(arrStr);
+    ASSERT_EQ(arrStr2.size(), 3);
+    EXPECT_EQ(arrStr2[0], "Hey");
+    EXPECT_EQ(arrStr2[1], ",");
+    EXPECT_EQ(arrStr2[2], "you");
+}
+//
+
+TEST(Array, Move_Operator)
+{
+    s21::array<int, 3> arrInt{1, 2, 3};
+    s21::array<int, 3> arrInt2;
+    arrInt2 = std::move(arrInt);
+
+    ASSERT_EQ(arrInt2.size(), 3);
+    ASSERT_EQ(arrInt2[0], 1);
+    ASSERT_EQ(arrInt2[1], 2);
+    ASSERT_EQ(arrInt2[2], 3);
+
+    s21::array<double, 3> arrDob{1.1, 2.2, 3.3};
+    s21::array<double, 3> arrDob2;
+    arrDob2 = std::move(arrDob);
+    ASSERT_EQ(arrInt2.size(), 3);
+    ASSERT_EQ(arrInt2[0], 1);
+    ASSERT_EQ(arrInt2[1], 2);
+    ASSERT_EQ(arrInt2[2], 3);
+
+    s21::array<std::string, 3> arrStr{"Hey", ",", "you"};
+    s21::array<std::string, 3> arrStr2;
+    arrStr2 = std::move(arrStr);
+    ASSERT_EQ(arrStr2.size(), 3);
+    EXPECT_EQ(arrStr2[0], "Hey");
+    EXPECT_EQ(arrStr2[1], ",");
+    EXPECT_EQ(arrStr2[2], "you");
+}
+
+TEST(Array, Copy_Constructor)
+{
+    s21::array<int, 3> arrInt{1, 2, 3};
+    s21::array<int, 3> arrInt2(arrInt);
+}
