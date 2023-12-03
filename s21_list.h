2e2897733374a94bf43c2c96948ad6112cc9a6ac
lana-cnmd <133262813+lana-cnmd@users.noreply.github.com>
2023-12-03 14:16:40 +0300

    first commit list

---
+++ s21_list.h
@@ -0,0 +1,9 @@
+#ifndef CPP2_S21_CONTEINERS_1_S21_LIST_H
+#define CPP2_S21_CONTEINERS_1_S21_LIST_H
+
+#include <iostream>
+
+namespace s21
+{
+} // namespaсe s21
+#endif // CPP2_S21_CONTEINERS_1_S21_LIST_H
---
+++ test_list.cc
@@ -0,0 +1,4 @@
+#include <gtest/gtest.h>
+
+#include "s21_list.h"
+#include <list>
